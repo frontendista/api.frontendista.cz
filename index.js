f42223ff86cdf53cfead6b09ebf19488e674d9f7
Pavel Sušický <susicky.pavel@outlook.cz>
2022-04-03 21:59:47 +0200

    stuff

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+addEventListener("fetch", event => {
+  event.respondWith(handleRequest(event.request));
+});
 
 /**
  * Respond with hello worker text
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  return new Response(
+    JSON.stringify({
+      hi: "hello worker",
+    }),
+    {
+      headers: { "content-type": "application/json" },
+    },
+  );
 }
