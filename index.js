eafd0230e83650ded907688a081784c869ae7a05
Pavel Sušický <susicky.pavel@outlook.cz>
2022-04-03 22:33:16 +0200

    feat: worker is working!

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+import Nope from "nope-validator";
+
 addEventListener("fetch", event => {
   event.respondWith(handleRequest(event.request));
+});
+
+const schema = Nope.object().shape({
+  name: Nope.string()
+    .min(1, "Name is too short")
+    .max(50, "Name is too long"),
+  email: Nope.string()
+    .max(100, "Email too long")
+    .email("Invalid email"),
+  message: Nope.string()
+    .min(1, "Message is too short")
+    .max(999, "Message is too long")
+    .required("Message is required"),
 });
 
 /**
@@ -7,6 +22,8 @@ addEventListener("fetch", event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
+  console.log(request.body);
+
   return new Response(
     JSON.stringify({
       hi: "hello worker",
